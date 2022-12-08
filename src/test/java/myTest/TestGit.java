af950d8faf9e1c6ca1a22a8ed8daae374f39ab0e
elshadYoll <elshad.shahverdi@gmail.com>
2022-12-08 02:10:02 -0500

    final test gor git Elshad

--- src/test/java/myTest/TestGit.java
+++ src/test/java/myTest/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		
+		String a="changes done for Git purpose, plz accept PR";
 		String test="test";
 System.out.println("TestGit");
 	}
